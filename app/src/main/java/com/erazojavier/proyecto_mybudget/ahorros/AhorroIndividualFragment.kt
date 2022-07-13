a01428c5993b729d6747f1c9d56b18ca91ca2ffb
Javier Erazo <alex.erazo01@epn.edu.ec>
2022-07-12 19:07:37 -0500

    update/ahorros individual GUI completado

--- app/src/main/java/com/erazojavier/proyecto_mybudget/ahorros/AhorroIndividualFragment.kt
+++ app/src/main/java/com/erazojavier/proyecto_mybudget/ahorros/AhorroIndividualFragment.kt
@@ -7,25 +7,29 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.erazojavier.proyecto_mybudget.R
+import com.erazojavier.proyecto_mybudget.databinding.FragmentAhorroIndividualBinding
 import com.erazojavier.proyecto_mybudget.databinding.FragmentAhorroResumenBinding
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
-class AhorrosResumenFragment : Fragment() {
 
-    private var _binding: FragmentAhorroResumenBinding? = null
+/**
+ * A simple [Fragment] subclass.
+ * Use the [AhorroIndividualFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class AhorroIndividualFragment : Fragment() {
+    private var _binding: FragmentAhorroIndividualBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentAhorroResumenBinding.inflate(inflater, container, false)
+        _binding = FragmentAhorroIndividualBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -33,12 +37,9 @@ class AhorrosResumenFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonNuevoAhorro.setOnClickListener {
-            findNavController().navigate(R.id.action_resumenAhorrosFragment_to_nuevoAhorroFragment)
-        }
-        binding.cardViewAhorrosInd2.setOnClickListener{
-            findNavController().navigate(R.id.action_resumenAhorrosFragment_to_ahorroIndividualFragment)
-        }
+//        binding.buttonNuevoAhorro.setOnClickListener {
+//            findNavController().navigate(R.id.action_resumenAhorrosFragment_to_nuevoAhorroFragment)
+//        }
     }
 
     override fun onDestroyView() {
